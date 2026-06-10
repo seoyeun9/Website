fd1856e10f96854cdec210f79e19ddd7a082ff92
seoyeun <seoyeun0315@gmail.com>
2026-06-10 11:17:05 +0100

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -47,5 +47,12 @@ document.addEventListener("DOMContentLoaded", () => {
     
     updateScrollProgress();
     
-    window.addEventListener('scroll', updateScrollProgress);
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 10) {
+            document.body.classList.add('scrolled');
+        } else {
+            document.body.classList.remove('scrolled');
+        }
+        updateScrollProgress();
+    });
 });
